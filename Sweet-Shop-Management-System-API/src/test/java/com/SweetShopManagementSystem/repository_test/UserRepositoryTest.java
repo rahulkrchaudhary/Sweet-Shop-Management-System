f086560e37ed3a403a14851ff114085e356cdd72
rahulkrchaudhary <rahulkrchaudhary12@gmail.com>
2025-09-18 18:24:07 +0530

    feat(User Repository): Test Passed - saving new user and finding same user by email [GREEN]

--- Sweet-Shop-Management-System-API/src/test/java/com/SweetShopManagementSystem/repository_test/UserRepositoryTest.java
+++ Sweet-Shop-Management-System-API/src/test/java/com/SweetShopManagementSystem/repository_test/UserRepositoryTest.java
@@ -1,17 +1,29 @@
 package com.SweetShopManagementSystem.repository_test;
 
 //import com.SweetShopManagementSystem.model.User;
+import com.SweetShopManagementSystem.model.User;
+import com.SweetShopManagementSystem.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
     @Test
     void saveAndFindByEmail() {
-        User u = new User(null, "Rahul", "r@example.com", "secret");
+        User u = new User();
+        u.setName("Rahul");
+        u.setEmail("r@example.com");
+        u.setPassword("secret");
         userRepository.save(u);
 
         Optional<User> found = userRepository.findByEmail("r@example.com");
