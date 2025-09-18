4993bb9f9bdf7a0c3479c77f15af2ea13fddfed2
rahulkrchaudhary <rahulkrchaudhary12@gmail.com>
2025-09-18 09:47:00 +0530

    Initial project structure

---
+++ Sweet-Shop-Management-System-API/src/main/java/com/SweetShopManagementSystem/SweetShopManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.SweetShopManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetShopManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetShopManagementSystemApiApplication.class, args);
+	}
+
+}
